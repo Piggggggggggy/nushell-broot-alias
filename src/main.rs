18819e8bb3c8b282f4384e32b670d5634f63c604
Ethan Shiota <54644546+Piggggggggggy@users.noreply.github.com>
2023-05-26 12:32:04 -0700

    removed quotes from output

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,16 @@ use std::process::{self, Stdio};
 use temp_file;
 fn main() {
     let temp = temp_file::empty();
-    let broot = process::Command::new("broot")
+    let _broot = process::Command::new("broot")
         .arg("--outcmd")
         .arg(temp.path())
         .stdout(Stdio::piped())
         .status();
-    println!("{}", std::fs::read_to_string(temp.path()).unwrap().trim());
+    println!(
+        "{}",
+        std::fs::read_to_string(temp.path())
+            .unwrap()
+            .trim()
+            .replace("\"", "")
+    );
 }
